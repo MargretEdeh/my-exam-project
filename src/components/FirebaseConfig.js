750b36d693af001d854273a27133f93cb9679030
Margret Edeh <sagaedeh@gmail.com>
2022-11-06 14:08:24 -0800

    edited the firebase file

--- src/components/FirebaseConfig.js
+++ src/components/FirebaseConfig.js
@@ -1,3 +1,26 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDIRrfdIZ0Z2Qm12Q_11mveai6jM_iDDEM",
+//   authDomain: "developers-arc.firebaseapp.com",
+//   projectId: "developers-arc",
+//   storageBucket: "developers-arc.appspot.com",
+//   messagingSenderId: "514878550515",
+//   appId: "1:514878550515:web:2189326b8d7c9890af2286"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -7,14 +30,15 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDIRrfdIZ0Z2Qm12Q_11mveai6jM_iDDEM",
-  authDomain: "developers-arc.firebaseapp.com",
-  projectId: "developers-arc",
-  storageBucket: "developers-arc.appspot.com",
-  messagingSenderId: "514878550515",
-  appId: "1:514878550515:web:2189326b8d7c9890af2286"
+  apiKey: "AIzaSyBiw2IWwMu7iILWF2G6CJSET07Ym9pdgZo",
+  authDomain: "dev-arc-51944.firebaseapp.com",
+  projectId: "dev-arc-51944",
+  storageBucket: "dev-arc-51944.appspot.com",
+  messagingSenderId: "949351648331",
+  appId: "1:949351648331:web:669b37af5faefd5c5711ce"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
