f1aa167a58d0487d08e6003316aa7148843b9f99
Margret Edeh <sagaedeh@gmail.com>
2023-01-08 23:25:21 -0800

    adjusted the account page

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,43 +1,51 @@
-// import { async } from '@firebase/util'
-// import React, { useEffect } from 'react'
-import {  Navigate } from 'react-router-dom'
-import { UserContext } from './AuthContext'
+import { UserContext } from "./AuthContext";
+import pic2 from './pic2.svg'
+
 
 export default function Account() {
   // const Navigate = useNavigate()
 
-    const { LogOut, user } =UserContext()
-//     useEffect(()=>{
-//       if(user === null){
-//   Navigate('/')
-// setTimeout(()=>{alert('login first')} ,1000)
-//       }
-//     }, [user ,Navigate])
-    const handleLogout = async () =>{
-        try{ await LogOut()
-
-        }catch(error){
-            console.log(error.message)
-        }
+  const { LogOut, user } = UserContext();
+  
+  const handleLogout = async () => {
+    try {
+      await LogOut();
+    } catch (error) {
+      console.log(error.message);
     }
-    
+  };
 
-    
-    const handlePremuim= ()=>{
-      alert(" May be in the future")
-    }
+  const handlePremuim = () => {
+    alert(" May be in the future");
+  };
   return (
-    <div className='logout'>
-      <h3>Always Remember to Sign in with {user.email} and keep Your details safe </h3>
-      <h4>Thanks for using this App</h4>
-      <div className='account'>
-            <img src={user.photoURL} className='google-pic' alt='profile pic'/>
-            <h2>{user.displayName} </h2>
+    <div className="logout">
+      <div className="dev">
+        {" "}
+        <h2 className="h2-out"> About Developer's Arc </h2>
+        <div>
+          <p className="p-out">
+            it's basically an app where you meet with other developers around
+            the world who have different skill sets. They share their details
+            and from one level of acquaintance to another, you begin to share
+            ideas and participate in paired programming, collaboration, open-sourcing, and increase your network. therefore creating a community for yourself🧡{" "}
+          </p>
+          <img className="out-pic" src={pic2} alt="design "/>
         </div>
-      
-      <button className='btn' onClick={handleLogout}> <b>Log Out</b> </button>
-      <button className='btn' onClick={handlePremuim} ><h2>Try Premuim Today</h2> </button>
-      
+      </div>
+
+      <div className="outtro">
+        <h3>Welcome {user.displayName}, </h3>
+      <p className="p-tag" >Always Remember to Sign in with <b> {user.email} </b> and keep Your details safe </p>
+      <h4>Thanks for using this App</h4>
+        <button className="btn-out" onClick={handleLogout}>
+          {" "}
+          <b>Log Out</b>{" "}
+        </button>
+        <button className="btn-out" onClick={handlePremuim}>
+          <h2>Try Premuim Today</h2>{" "}
+        </button>
+      </div>
     </div>
-  )
+  );
 }
